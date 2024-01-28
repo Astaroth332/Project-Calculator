78aee9679e7750ba1373d44d9c0e36ce4f9492de
Astaroth332 <nomairiguro66@gmail.com>
2024-01-28 18:14:01 +0800

    Fixing the error of deletition of zero if it is the first input number and added the feature such as delete button, dot button, handling the error if divide by 0

--- script.js
+++ script.js
@@ -34,10 +34,11 @@ function operate(num1, operator, num2) {
 
 const display = document.querySelector('#display');
 
-
 function addToDisplay(input) {
    display.value += input;
 }
+
+addToDisplay("0")
 
 const clear = document.querySelector('#clear');
 const equal = document.querySelector('#equal');
@@ -48,40 +49,77 @@ let firstOperand = "";
 let secondOperand = "";
 let operator = "";
 let isNewNum = false;
-let clearDisplay = false;
+let clearDisplayAfterOperator = false;
 let result = '';
-
-
-function initialZeroValue() {
-    const input = document.querySelector('#display');
-    input.value = "0";
-}
-
-initialZeroValue();
+let clearInitialValue = true;
+let clearFirstNumberIfZeroForFirstOperand = true;
+let clearFirstNumberIfZeroForSecondOperand = true;
+let containOneDotForFirstOperand = false;
+let containOneDotForSecondOperand = false;
+let checkDotForFirstOperand = true;
+let checkDotForSecondOperand = false;
+let activateDeleteInFirstOperand = false;
+let activateDeleteInSecondOperand = false;
+let activateDeleteInResult = false;
 
 buttonsForNumbers.forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
 
-        if (button.id == 0 && firstOperand.length == 0) {
-            initialZeroValue();
-        }else {
-       
-   
-        if (clearDisplay) {
+        if (e.target.id === "." && checkDotForFirstOperand) {
+            if (containOneDotForFirstOperand) {
+            return;
+        }
+            containOneDotForFirstOperand = true;
+        }
+        
+        if (checkDotForSecondOperand && e.target.id === ".") {
+                if (containOneDotForSecondOperand) {
+                    return;
+            }
+            containOneDotForSecondOperand = true;
+        }
+
+        if (clearDisplayAfterClearButton) {
             display.value = "";
-            clearDisplay = false;
+            clearDisplayAfterClearButton = false;
+        }
+
+        if (clearValueIfIncomplete) {
+            display.value = "";
+            clearValueIfIncomplete = false;
+        }
+
+        if (firstOperand.charAt(0) === "0" && clearFirstNumberIfZeroForFirstOperand ) {
+            display.value = "";     
+            clearFirstNumberIfZeroForFirstOperand = false;
+        }
+
+        if (secondOperand.charAt(0) === "0" && clearFirstNumberIfZeroForSecondOperand) {
+            display.value = "";
+            clearFirstNumberIfZeroForSecondOperand = false;
+        }
+
+
+        if (clearInitialValue) {
+            display.value = "";
+            clearInitialValue = false;
+        }
+
+        if (clearDisplayAfterOperator) {
+            display.value = "";
+            clearDisplayAfterOperator = false;
         }
 
         if (isNewNum) {     
             secondOperand += button.id;
             addToDisplay(button.id)
+            activateDeleteInFirstOperand = false;
+            activateDeleteInSecondOperand = true;
         }   else {
             firstOperand += button.id;
             addToDisplay(button.id);
+            activateDeleteInFirstOperand = true;
         }
-
-    }
-
 
     });
 });
@@ -89,13 +127,21 @@ buttonsForNumbers.forEach((button) => {
 const buttonForOperatorContainer = document.querySelector('.button-operator');
 const buttonForOperator = buttonForOperatorContainer.querySelectorAll('button'); 
 
+let specialCaseForDivide = false;
 
 buttonForOperator.forEach(button => {
     button.addEventListener('click', () => {
         isNewNum = true;
-        clearDisplay = true;
+        clearDisplayAfterOperator = true;
+        checkDotForSecondOperand = true;
+        checkDotForFirstOperand = false;
 
         if (firstOperand !== "" && operator !== "" && secondOperand !== "") {
+
+            if (specialCaseForDivide && parseFloat(secondOperand) === 0 ) {
+                display.value = "NOPE! CANNOT DIVIDE BY ZERO";
+                return;
+            }
     
             result = String(operate(parseFloat(firstOperand), operator, 
             parseFloat(secondOperand)));
@@ -104,7 +150,8 @@ buttonForOperator.forEach(button => {
 
             firstOperand = result;
             secondOperand = "";
-          
+            activateDeleteInSecondOperand = false;
+            
         } 
 
         operator = button.id;
@@ -118,6 +165,7 @@ buttonForOperator.forEach(button => {
                 break;
             case "div":
                 operator = "/";
+                specialCaseForDivide = true;
                 break;
             case "mul":
                 operator = "*";
@@ -125,27 +173,98 @@ buttonForOperator.forEach(button => {
         }
     });
 });
-
+let clearValueIfIncomplete = false;
 
 equal.addEventListener('click', () => {
 
-    if (firstOperand == "" || secondOperand == "" || operator == "") {
-    initialZeroValue()
-    } else {
 
+    if (specialCaseForDivide && parseFloat(secondOperand) === 0) {
+        display.value = "NOPE! CANNOT DIVIDE BY ZERO";
+        return;
+    }
+
+    if (firstOperand == "" && secondOperand == "" && operator == "") {
+        display.value = ""
+        addToDisplay("0")
+        clearValueIfIncomplete = true;
+    } else if (operator == "" && secondOperand == "") {
+        display.value = ""
+        addToDisplay("0")
+        firstOperand = "";
+        clearValueIfIncomplete = true;
+    } else if(secondOperand == "") {
+        display.value = ""
+        addToDisplay("0")
+        firstOperand = "";
+        operator = "";
+        clearValueIfIncomplete = true; 
+        isNewNum = false;
+    }else{
     display.value = "";
     result = String(operate(parseFloat(firstOperand), operator, 
     parseFloat(secondOperand)));
     let convertToNumber = Number(result);
     addToDisplay(+(Math.round(convertToNumber + "e+2") + "e-2"));
+    activateDeleteInSecondOperand = false;
+    }
+});
+
+let clearDisplayAfterClearButton = false;
+
+clear.addEventListener('click' , () => {
+    if (firstOperand == "" && secondOperand == "" && operator == "") {
+        display.value = ""
+        addToDisplay("0")
+        clearDisplayAfterClearButton = true;
+    } else if (operator == "" && secondOperand == "") {
+        display.value = ""
+        addToDisplay("0")
+        firstOperand = "";
+        clearDisplayAfterClearButton = true;
+    } else if(secondOperand == "") {
+        display.value = ""
+        addToDisplay("0")
+        firstOperand = "";
+        operator = "";
+        clearDisplayAfterClearButton = true; 
+        isNewNum = false;
+    } else {
+        display.value = "";
+        addToDisplay("0")
+        firstOperand = ""
+        secondOperand = ""
+        operator = ""
+        clearDisplayAfterClearButton = true; 
+        isNewNum = false;
+    }
+
+    
+});
+
+
+const buttonForDelete = document.querySelector('#delete');
+
+buttonForDelete.addEventListener('click' , () => {
+
+    if(activateDeleteInFirstOperand) {
+        display.value = "";
+        let modifiedFirstOperand = firstOperand.slice(0,-1);
+        addToDisplay(modifiedFirstOperand);
+        firstOperand = modifiedFirstOperand;
+    }else if (activateDeleteInSecondOperand) {
+        display.value = "";
+        let modifiedSecondOperand = secondOperand.slice(0,-1);
+        addToDisplay(modifiedSecondOperand);
+        secondOperand = modifiedSecondOperand;
+    }   else {
+        display.value = "";
+        let modifiedResult = result.slice(0,-1);
+        addToDisplay(modifiedResult);
+        firstOperand = modifiedResult;
+        secondOperand = "";
+        isNewNum = true;
     }
 })
-
-
-
-
-
-
 
 
 
